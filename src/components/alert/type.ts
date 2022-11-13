13df18c90a2fab6fceef30b28d099164c25044c8
MakiMarc <marc@keleya.de>
2022-11-13 17:50:54 +0100

    removed not used attribute "size" for alert and repaired unit test

--- src/components/alert/type.ts
+++ src/components/alert/type.ts
@@ -1,9 +1,7 @@
-import { I_Ui_Sizes } from './../_types/sizes/sizes'
 import { I_Variants } from '../_types/colors/ui_variants'
 import { HTMLProps, MouseEventHandler } from 'react'
 
 export interface Ui_AlertProps extends I_Variants, Omit<HTMLProps<HTMLDivElement>, 'size'> {
-  size?: I_Ui_Sizes
   noIcon?: boolean
   onDenyButtonText?: string
   onAcceptButtonText?: string
