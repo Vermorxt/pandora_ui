b49512865c14260d36e4fb62cb9a651ef98f1ab4
MakiMarc <marc@keleya.de>
2022-11-26 09:47:42 -0600

    flags

--- tailwind.config.js
+++ tailwind.config.js
@@ -43,6 +43,7 @@ module.exports = {
         'w-{width}',
         'h-{height}',
         'text-{colors}',
+        'rounded-{size}',
         'btn-{colors}',
         'bg-{colors}',
         'bg-{colors}-focus',
